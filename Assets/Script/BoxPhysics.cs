d6361b66f0cca46320908227e2724d2131d3a521
German-99 <42181365+German-99@users.noreply.github.com>
2019-02-20 08:54:40 -0700

    Initial commit

---
+++ Assets/Script/BoxPhysics.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPhysics : MonoBehaviour {
+
+    Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.AddForce(Vector2.up * 1000, ForceMode2D.Force);
+    }
+}
